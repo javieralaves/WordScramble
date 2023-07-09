1376c8cdd0cd04b249c26c9eaadd8c9ac5759637
Javier Alaves <jayalaves@gmail.com>
2023-07-09 17:01:36 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Javier Alaves on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
